6af8da1a7b77a2bf9fc84dabe0b422ed8bd75151
subramaniyan <subramaniyan@webizze.com>
2025-12-15 08:32:10 +0530

    added the ignore logs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
 import { persistor, store } from "@/redux/store";
 import { InitialLayout } from "@/src/components";
+import { LogBox } from 'react-native';
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+
+LogBox.ignoreAllLogs();
 export default function RootLayout() {
+  
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
